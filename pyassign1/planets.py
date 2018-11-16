e4a796e369ec6c3824fb79784ca4470f493dceff
贺泽宇 <1800011821@pku.edu.cn>
2018-11-16 12:27:03 +0800

    Update planets.py

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -17,14 +17,18 @@ h=[a,b,c,d,e,f]
 a.dot(80,"yellow")#太阳的大小与颜色
 colors=['blue','green','red','black','orange','cornflowerblue'] #行星的颜色
 x=0
-while x>=-1:
-    s=h[x%6]
-    s.speed(0)
-    s.shape('circle')
-    s.pensize(7)
-    s.color(colors[x%6])
-    s.penup()
-    s.goto((1+x%6)*50*math.sin(x/(1000*(1+x%6))),(1+x%6)*60*math.cos(x/(1000*(1+x%6))))
-    s.pendown()
-    s.goto((1+x%6)*50*math.sin((x+1)/(100*(1+x%6))),(1+x%6)*60*math.cos((x+1)/(1000*(1+x%6))))
-    x=x+1
+def main():
+    global x
+    while x>=-1:
+        s=h[x%6]
+        s.speed(0)
+        s.shape('circle')
+        s.pensize(7)
+        s.color(colors[x%6])
+        s.penup()
+        s.goto((1+x%6)*50*math.sin(x/(1000*(1+x%6))),(1+x%6)*60*math.cos(x/(1000*(1+x%6))))
+        s.pendown()
+        s.goto((1+x%6)*50*math.sin((x+1)/(100*(1+x%6))),(1+x%6)*60*math.cos((x+1)/(1000*(1+x%6))))
+        x=x+1
+if __name__ == '__main__':
+    main()      
