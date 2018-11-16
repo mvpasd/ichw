1a85ffd81e6dd386a66ba876df1216b463f242c1
贺泽宇 <1800011821@pku.edu.cn>
2018-11-16 12:19:45 +0800

    Update planets.py

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -14,7 +14,7 @@ e.shapesize(2.1)
 f=turtle.Turtle()
 f.shapesize(2.5)
 h=[a,b,c,d,e,f]
-a.dot(40,"yellow")#太阳的大小与颜色
+a.dot(80,"yellow")#太阳的大小与颜色
 colors=['blue','green','red','black','orange','cornflowerblue'] #行星的颜色
 x=0
 while x>=-1:
@@ -24,7 +24,7 @@ while x>=-1:
     s.pensize(7)
     s.color(colors[x%6])
     s.penup()
-    s.goto((1+x%6)*50*math.sin(x/(100*(1+x%6))),(1+x%6)*60*math.cos(x/(100*(1+x%6))))
+    s.goto((1+x%6)*50*math.sin(x/(1000*(1+x%6))),(1+x%6)*60*math.cos(x/(1000*(1+x%6))))
     s.pendown()
-    s.goto((1+x%6)*50*math.sin((x+1)/(100*(1+x%6))),(1+x%6)*60*math.cos((x+1)/(100*(1+x%6))))
+    s.goto((1+x%6)*50*math.sin((x+1)/(100*(1+x%6))),(1+x%6)*60*math.cos((x+1)/(1000*(1+x%6))))
     x=x+1
